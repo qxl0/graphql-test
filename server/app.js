2e3e1b845db762aa12bc67f6f8a898cae6687936
Qiang <qxl0@yahoo.com>
2021-11-26 20:20:23 -0500

    finish mutations

--- server/app.js
+++ server/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
-
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql');
+const schema = require('./schema');
+const mongoose = require('mongoose');
 const app = express();
+
+// connect to database
+mongoose.connect("mongodb+srv://qiang:qiangli2@cluster0.mqpni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+var root = {
+    hello: () => {
+        return 'Hello World!';
+    }
+};
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 app.listen(4000, () => {
     console.log('now listening for requests on port 4000');
